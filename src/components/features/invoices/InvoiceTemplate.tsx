1468aee17964726b7c186797990e352a866e0f96
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2026-09-05 20:31:39 +0600

    edited invoice

--- src/components/features/invoices/InvoiceTemplate.tsx
+++ src/components/features/invoices/InvoiceTemplate.tsx
@@ -113,15 +113,7 @@ export default function InvoiceTemplate({
               ))}
               <tr>
                 <td colSpan={6} className="py-3 px-3 border border-black">
-                  <div className="flex justify-between items-start">
-                    <div className="text-left w-1/2 pr-4">
-                      {data.notes && (
-                        <div className="text-sm">
-                          <span className="font-semibold underline">Notes:</span>
-                          <p className="whitespace-pre-wrap mt-1">{data.notes}</p>
-                        </div>
-                      )}
-                    </div>
+                  <div className="flex justify-end items-start">
                     <div className="text-right space-y-2">
                       {data.subtotal > data.total && (
                         <>
@@ -148,7 +140,12 @@ export default function InvoiceTemplate({
                             {(data.total - data.dueAmount).toLocaleString()} Tk
                           </div>
                           <div className="text-base">
-                            <span className="font-semibold">{data.dueType === "installment" ? "Installment" : "Due"} :</span>{" "}
+                            <span className="font-semibold">
+                              {data.dueType === "installment"
+                                ? "Installment"
+                                : "Due"}{" "}
+                              :
+                            </span>{" "}
                             <span className="font-bold">
                               {data.dueAmount.toLocaleString()} Tk
                             </span>
@@ -170,6 +167,13 @@ export default function InvoiceTemplate({
               </tr>
             </tbody>
           </table>
+          <div className="text-center w-full py-3">
+            {data.notes && (
+              <div className="text-sm">
+                <p className="whitespace-pre-wrap mt-1">{data.notes}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/db/migrations/0004_worried_butterfly.sql
+++ src/db/migrations/0004_worried_butterfly.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "banners" (
+CREATE TABLE IF NOT EXISTS "banners" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"imageKey" varchar(255) NOT NULL,
 	"isActiveCustomer" boolean DEFAULT false NOT NULL,
